befe13630044e4f08b2edd5eef47c76838b9b53d
Efstathios Dou <doustathis@gmail.com>
2022-09-24 16:48:00 +0300

    sep script & dir png

--- PA1_Separate_RScript.R
+++ PA1_Separate_RScript.R
@@ -1,87 +1,34 @@
----
-title: 'Reproducible Research: Peer Assessment 1'
-output:
-  html_document:
-    keep_md: yes
-  pdf_document: default
----
+#PA1-RScript
 
-## Loading and preprocessing the data
-
-
-```r
 unzip("activity.zip")                     #unzip the zip file with our data
 bufferdata<-read.csv("./activity.csv")    #read our data
-```
 
-## What is mean total number of steps taken per day?
-
-
-```r
 library(ggplot2)                          #load ggplot2 package
 #Create a data frame with step summaries
 histdata<-data.frame(with(bufferdata,tapply(bufferdata$steps,bufferdata$date,FUN=sum, na.rm=TRUE)))
 #Change col name
 colnames(histdata)<-c("steps")
 #Create histogram plot
+png(filename = "plot1_stepsum.png", width = 480, height = 480, units = "px",bg = "white")
 qplot(histdata$steps, binwidth = 1200, xlab = "(steps number) / (day)",fill = I("lightsalmon2"),ylim=c(0,11),colour=I("red"))
-```
-
-![](PA1_template_files/figure-html/unnamed-chunk-2-1.png)<!-- -->
-
-```r
+dev.off()
 #Get mean
 paste("mean steps : ", mean(histdata$steps))
-```
-
-```
-## [1] "mean steps :  9354.22950819672"
-```
-
-```r
 #Get median
 paste("median steps : ", median(histdata$steps))
-```
 
-```
-## [1] "median steps :  10395"
-```
-
-## What is the average daily activity pattern?
-
-
-```r
 #Aggregate mean steps by interval
 stepsinv<-aggregate(steps ~ interval, bufferdata, mean)
 #Plot for interval and steps on aggregated table
+png(filename = "plot2_intervals.png", width = 480, height = 480, units = "px",bg = "white")
 ggplot(data = stepsinv, aes(x = interval, y = steps, color="red")) + geom_line() + xlab("5 minute interval") + ylab("average number of steps taken")
-```
-
-![](PA1_template_files/figure-html/unnamed-chunk-3-1.png)<!-- -->
-
-```r
+dev.off()
 #Get interval with most average steps and the average for it
 stepsinv[which.max(stepsinv$steps), ]
-```
 
-```
-##     interval    steps
-## 104      835 206.1698
-```
-
-## Imputing missing values
-
-
-```r
 #In order to fill the missing values we'll first check how many of them we have
 paste("Rows with NA are", sum(is.na(bufferdata$steps)))
-```
 
-```
-## [1] "Rows with NA are 2304"
-```
-
-```r
 #We replace NA values with mean value
 tempvec<-(replace(bufferdata$steps, is.na(bufferdata$steps), mean(bufferdata$steps, na.rm = TRUE)))
 #clone the bufferdata
@@ -89,35 +36,18 @@ imputeddata<-bufferdata
 #replace steps with imputed steps (NA-mean)
 imputeddata$steps<-tempvec
 #Re-create our histogram with imputed date
+
 histdataQ<-data.frame(with(imputeddata,tapply(imputeddata$steps,imputeddata$date,FUN=sum, na.rm=TRUE)))
 colnames(histdataQ)<-c("steps")
+png(filename = "plot3_imputed.png", width = 480, height = 480, units = "px",bg = "white")
 qplot(histdataQ$steps, binwidth = 1200, xlab = "(steps number) / (day)",fill = I("lightsalmon2"),ylim=c(0,11),colour=I("red"))
-```
+dev.off()
 
-![](PA1_template_files/figure-html/unnamed-chunk-4-1.png)<!-- -->
-
-```r
 #Re-calculate mean with imputed values
 paste("mean steps : ", mean(histdataQ$steps))
-```
-
-```
-## [1] "mean steps :  10766.1886792453"
-```
-
-```r
 #Re-calculate median with imputed values
 paste("median steps : ", median(histdataQ$steps))
-```
 
-```
-## [1] "median steps :  10766.1886792453"
-```
-
-## Are there differences in activity patterns between weekdays and weekends?
-
-
-```r
 #Save imputed data on a new table
 hd1<-imputeddata
 #Converting date col to date and then to weekday
@@ -126,15 +56,11 @@ hd1$date<-weekdays(as.Date(hd1$date))
 hd1$date <- ifelse(hd1$date == "Saturday"|hd1$date=="Sunday", "Weekend", "Weekday")   
 #Create an aggregated table by (week(day/end)-factor and interval
 meanfactorday<-aggregate(steps ~ interval+date, hd1, mean)
-#Comparison side by side (vertical)
-ggplot(data = meanfactorday, aes(x = interval, y = steps,color=date)) + geom_line()+facet_grid(date~.) + xlab("5 minute interval") + ylab("average number of steps taken")
-```
-
-![](PA1_template_files/figure-html/unnamed-chunk-5-1.png)<!-- -->
-
-```r
+#Comparison side by side
+png(filename = "plot4_sidebyside.png", width = 480, height = 480, units = "px",bg = "white")
+ggplot(data = meanfactorday, aes(x = interval, y = steps,color=date)) + geom_line()+facet_grid(.~date) + xlab("5 minute interval") + ylab("average number of steps taken")
+dev.off()
 #Comparison on same plot
+png(filename = "plot5_combined.png", width = 480, height = 480, units = "px",bg = "white")
 ggplot(data = meanfactorday, aes(x = interval, y = steps,color=date)) + geom_line() + xlab("5 minute interval") + ylab("average number of steps taken")
-```
-
-![](PA1_template_files/figure-html/unnamed-chunk-5-2.png)<!-- -->
+dev.off()
